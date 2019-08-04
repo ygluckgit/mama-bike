2553f6e85e10de6e6c45fbe60c7bdd46520113e1
ygluckgit <451621312@QQ.com>
2019-08-04 21:05:13 +0800

    add TestGit

---
+++ src/main/java/com/yg/demo/user/entity/TestGit.java
@@ -0,0 +1,13 @@
+package com.yg.demo.user.entity;
+
+/**
+ * @ClassName TestGit
+ * @Description TODO
+ * @Author YGuang
+ * @Date 2019/8/4 20:57
+ * @Version 1.0
+ **/
+
+
+public class TestGit {
+}
